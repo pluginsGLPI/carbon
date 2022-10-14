b7013fe4ade1ce96f8ea45078ba43658dd6f46f1
François Déchelle <fdechelle@teclib.com>
2022-10-14 12:30:10 +0200

    Move static methods

--- src/Dashboard.php
+++ src/Dashboard.php
@@ -3,6 +3,8 @@
 namespace GlpiPlugin\Carbon;
 
 use GlpiPlugin\Carbon\Power;
+use ComputerModel;
+use Computer;
 
 class Dashboard
 {
@@ -37,7 +39,7 @@ class Dashboard
         $params = array_merge($default_params, $params);
 
         return [
-            'number' => Power::getTotalPower(),
+            'number' => self::getTotalPower(),
             'label'  => $params['label'],
             'icon'  => $params['icon'],
         ];
@@ -53,10 +55,93 @@ class Dashboard
         $params = array_merge($default_params, $params);
 
         return [
-            'data' => Power::getPowerPerModel(),
+            'data' => self::getPowerPerModel(),
             'label'  => $params['label'],
             'icon'  => $params['icon'],
             'color' => $params['color'],
         ];
+    }
+    /**
+     * Returns total power of all computers.
+     * 
+     * @return int: total power of all computers
+     * @deprecated
+     */
+    static function getTotalPower()
+    {
+        global $DB;
+
+        $computers_table = Computer::getTable();
+        $computermodels_table = ComputerModel::getTable();
+
+        $result = $DB->request([
+            'SELECT'    => [
+                'SUM' => 'power_consumption AS total_power_consumption'
+            ],
+            'FROM'      => $computermodels_table,
+            'INNER JOIN' => [
+                $computers_table => [
+                    'FKEY'   => [
+                        $computermodels_table  => 'id',
+                        $computers_table => 'computermodels_id',
+                    ]
+                ]
+            ]
+        ]);
+        if ($row = $result->current()) {
+            $total_power_consumption = $row['total_power_consumption'];
+            return $total_power_consumption;
+        }
+
+        return 42;
+    }
+
+    /**
+     * Returns total power per computer model.
+     * 
+     * @return array of:
+     *   - int  'number': total power of the model
+     *   - string 'url': url to redirect when clicking on the slice
+     *   - string 'label': name of the computer model
+     * @deprecated
+     */
+    static function getPowerPerModel()
+    {
+        global $DB;
+
+        $computers_table = Computer::getTable();
+        $computermodels_table = ComputerModel::getTable();
+
+        $result = $DB->request([
+            'SELECT'    => [
+                ComputerModel::getTableField('name'),
+                'SUM' => 'power_consumption AS power_consumption_per_model',
+                ComputerModel::getTableField('id'),
+            ],
+            'FROM'      => $computermodels_table,
+            'INNER JOIN' => [
+                $computers_table => [
+                    'FKEY'   => [
+                        $computermodels_table  => 'id',
+                        $computers_table => 'computermodels_id',
+                    ]
+                ]
+            ],
+            'WHERE' => [
+                'power_consumption' => ['>', '0'],
+            ],
+            'GROUPBY' => ComputerModel::getTableField('id'),
+        ]);
+
+        $data = [];
+        foreach ($result as $row) {
+            $data[] = [
+                'number' => $row['power_consumption_per_model'],
+                'url' => '/front/computermodel.form.php?id=' . $row['id'],
+                'label' => $row['name'],
+            ];
+        }
+
+        return $data;
     }
 }
