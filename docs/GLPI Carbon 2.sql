862cb9764d8f0ddc67091e211b1fa8bf231f746f
François Déchelle <fdechelle@teclib.com>
2022-10-20 11:34:02 +0200

    Update schema

--- docs/GLPI Carbon 2.sql
+++ docs/GLPI Carbon 2.sql
@@ -22,6 +22,28 @@ CREATE TABLE `PowerModel_ComputerModel` (
   `powermodels_id` int
 );
 
+CREATE TABLE `ElectricityMapZone` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `latitude` float,
+  `longitude` float,
+  `zone` varchar(255),
+  `zone_name` varchar(255),
+  `country_name` varchar(255)
+);
+
+CREATE TABLE `ElectricityMapZone_Computer` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `electricitymapzone_id` int,
+  `computers_id` int
+);
+
+CREATE TABLE `CarbonEmission` (
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `emission` float,
+  `emission_date` datetime,
+  `computers_id` int
+);
+
 CREATE TABLE `ComputerModel` (
   `id` int PRIMARY KEY AUTO_INCREMENT
 );
@@ -31,6 +53,10 @@ CREATE TABLE `Computer` (
   `computermodels_id` int
 );
 
+ALTER TABLE `ComputerModel` COMMENT = 'This table is GLPI native';
+
+ALTER TABLE `Computer` COMMENT = 'This table is GLPI native';
+
 ALTER TABLE `Power` ADD FOREIGN KEY (`computers_id`) REFERENCES `Computer` (`id`);
 
 ALTER TABLE `PowerModel` ADD FOREIGN KEY (`powermodelcategories_id`) REFERENCES `PowerModelCategory` (`id`);
@@ -38,5 +64,11 @@ ALTER TABLE `PowerModel` ADD FOREIGN KEY (`powermodelcategories_id`) REFERENCES 
 ALTER TABLE `PowerModel_ComputerModel` ADD FOREIGN KEY (`computermodels_id`) REFERENCES `ComputerModel` (`id`);
 
 ALTER TABLE `PowerModel_ComputerModel` ADD FOREIGN KEY (`powermodels_id`) REFERENCES `PowerModel` (`id`);
+
+ALTER TABLE `ElectricityMapZone_Computer` ADD FOREIGN KEY (`electricitymapzone_id`) REFERENCES `ElectricityMapZone` (`id`);
+
+ALTER TABLE `ElectricityMapZone_Computer` ADD FOREIGN KEY (`computers_id`) REFERENCES `Computer` (`id`);
+
+ALTER TABLE `CarbonEmission` ADD FOREIGN KEY (`computers_id`) REFERENCES `Computer` (`id`);
 
 ALTER TABLE `Computer` ADD FOREIGN KEY (`computermodels_id`) REFERENCES `ComputerModel` (`id`);
