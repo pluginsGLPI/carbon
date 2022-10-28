0ef5feb466040a918bc3ab1794752989c7849c0c
François Déchelle <fdechelle@teclib.com>
2022-10-28 15:53:38 +0200

    Add API call, returned value is not correct

--- scripts/testCarbonDataProviderFrance.php
+++ scripts/testCarbonDataProviderFrance.php
@@ -5,3 +5,13 @@ include_once('../src/CarbonDataProvider.php');
 include_once('../src/CarbonDataProviderFrance.php');
 
 $provider = new GlpiPlugin\Carbon\CarbonDataProviderFrance();
+$query_params = [
+    'select'    => 'taux_co2,date_heure',
+    'where'     => 'date_heure IN [date\'2021-10-24T14:00:00+00:00\' TO date\'2021-10-24T15:00:00+00:00\']',
+    'order_by'  => 'date_heure desc',
+    'limit'     => 20,
+    'offset'    => 0,
+    'timezone'  => 'UTC'
+];
+
+print_r($provider->httpQuery('', $query_params));
