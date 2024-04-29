b8cd6f74512638a0e04bbe3af38f8c720712db80
Mary-Clb <mbressy@teclib.com>
2024-04-29 16:20:36 +0200

    implements fetch to providers & test graph lib

---
+++ ajax/totalCarbonEmissionPerModel.php
@@ -0,0 +1,12 @@
+<?php
+
+use GlpiPlugin\Carbon\Dashboard;
+
+include('../../../inc/includes.php');
+
+header("Content-Type: text/html; charset=UTF-8");
+
+$res = Dashboard::getTotalCarbonEmissionPerModel();
+Toolbox::logDebug($res);
+
+echo json_encode($res);
