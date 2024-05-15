980c4a2987160c00868559795d8f54f0a304f164
Mary-Clb <mbressy@teclib.com>
2024-05-15 13:57:41 +0200

    set webpack config to handle css and pics

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@
 import ApexCharts from "apexcharts";
 window.ApexCharts = ApexCharts;
 
-
-// Import images paths :
-require('../pics/illustration_bridge.png');
-require('../pics/carbonemissionicon.png');
-require('../pics/powerconsumptionicon.png');
-
 // Import css :
 require('../css/main.css');
+// Import css :
+require('../css/main.css');
+// Import images paths :
+require('../pics/illustration_bridge.png');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    // watch: true,
     plugins: [new MiniCssExtractPlugin()],
     entry: path.resolve(__dirname, './js/main.js'),
     mode: 'production',
@@ -21,6 +20,24 @@ module.exports = {
                 type: 'asset/resource',
                 generator: {
                     filename: 'images/[name][ext]',
+                },
+            },
+    // Config for css loading
+            {
+                test : /\.css$/,
+                use : [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ],
+    },
+
+    // Config for pics loading
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[hash][ext][query]',
                 },
             },
     // Config for css loading
