3a970ce4b4367d7884e7d507581bb0824ac9995d
Mary-Clb <mbressy@teclib.com>
2024-05-16 16:35:37 +0200

    fix webpack conf for loading pics

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ import ApexCharts from "apexcharts";
 window.ApexCharts = ApexCharts;
 
 
-// Import css :
-require('../css/main.css');
 // Import images paths :
 require('../pics/illustration_bridge.png');
+require('../pics/carbonemissionicon.png');
+require('../pics/powerconsumptionicon.png');
 
+// Import css :
+require('../css/main.css');
