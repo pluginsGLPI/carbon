47530a63856d1520f106a7ea8b165668529408ff
Mary-Clb <mbressy@teclib.com>
2024-05-23 12:07:43 +0200

    add footer and align page

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 import ApexCharts from "apexcharts";
 window.ApexCharts = ApexCharts;
 
-
 // Import images paths :
 require('../pics/illustration_bridge.png');
 require('../pics/carbon_emission_icon_darkgreen.png');
 require('../pics/carbon_emission_icon_white.png');
 require('../pics/ecology_icon_orange.png');
+require('../pics/illustration-footer.png');
 
 // Import css :
 require('../css/main.css');
