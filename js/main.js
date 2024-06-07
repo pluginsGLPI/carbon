014256f70bd11428fe94090126cd1e134a12432e
mbressy <bressy.marie@gmail.com>
2024-06-07 13:35:45 +0200

    set header img in png & modify block height

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ window.ApexCharts = ApexCharts;
 
 // Import images paths :
 require('../pics/illustration_bridge.png');
+require('../pics/illustration-bridge-green.png');
 require('../pics/carbon_emission_icon_darkgreen.png');
 require('../pics/carbon_emission_icon_white.png');
 require('../pics/ecology_icon_orange.png');
+require('../pics/ecology-icon-light.png');
 require('../pics/illustration-footer.png');
 require('../pics/device_icon_black.png');
 
