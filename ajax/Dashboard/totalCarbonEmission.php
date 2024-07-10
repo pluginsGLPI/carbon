33a0dc6a4d83e2716b4b5975e214edb44e72b781
mbressy <bressy.marie@gmail.com>
2024-07-10 15:49:52 +0200

    move ajax files - activate providers for cards

--- ajax/Dashboard/totalCarbonEmission.php
+++ ajax/Dashboard/totalCarbonEmission.php
@@ -33,19 +33,10 @@
 
 use GlpiPlugin\Carbon\Dashboard\Dashboard;
 
-include('../../../inc/includes.php');
+include('../../../../inc/includes.php');
 
 header("Content-Type: text/html; charset=UTF-8");
 
-$res = Dashboard::getTotalCarbonEmissionPerModel();
+$res = Dashboard::getTotalCarbonEmission();
 
-// Generate fake data
-$fakeRes = [
-    'desktop' => number_format((float)rand(1001, 2999) / 1000, 3),
-    'tablets' => number_format((float)rand(1001, 2999) / 1000, 3),
-    'laptops' => number_format((float)rand(1001, 2999) / 1000, 3),
-    'mobiles' => number_format((float)rand(1001, 2999) / 1000, 3),
-    'others' => number_format((float)rand(1001, 2999) / 1000, 3)
-];
-
-echo json_encode($fakeRes);
+echo json_encode($res);
--- ajax/Dashboard/totalCarbonEmissionPerModel.php
+++ ajax/Dashboard/totalCarbonEmissionPerModel.php
@@ -33,12 +33,19 @@
 
 use GlpiPlugin\Carbon\Dashboard\Dashboard;
 
-include('../../../inc/includes.php');
+include('../../../../inc/includes.php');
 
 header("Content-Type: text/html; charset=UTF-8");
 
-$res = Dashboard::getTotalCarbonEmission();
+$res = Dashboard::getTotalCarbonEmissionPerModel();
 
 // Generate fake data
-$fakeRes = number_format((float)rand(1001, 2999) / 1000, 3);
+$fakeRes = [
+    'desktop' => number_format((float)rand(1001, 2999) / 1000, 3),
+    'tablets' => number_format((float)rand(1001, 2999) / 1000, 3),
+    'laptops' => number_format((float)rand(1001, 2999) / 1000, 3),
+    'mobiles' => number_format((float)rand(1001, 2999) / 1000, 3),
+    'others' => number_format((float)rand(1001, 2999) / 1000, 3)
+];
+
 echo json_encode($fakeRes);
