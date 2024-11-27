0d7651cf1ced385ebd29e3be271d03043e0454ac
Thierry Bugier <tbugier@teclib.com>
2024-11-27 12:09:35 +0100

    test: various test fixes

--- install/mysql/plugin_carbon_empty.sql
+++ install/mysql/plugin_carbon_empty.sql
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_carbonintensitysources_carboninte
   `plugin_carbon_carbonintensitysources_id` int unsigned NOT NULL DEFAULT '0',
   `plugin_carbon_carbonintensityzones_id`   int unsigned NOT NULL DEFAULT '0',
   `code`                                    varchar(255) DEFAULT NULL         COMMENT 'Zone identifier in the API of the source',
-  `is_download_enabled`                     tinyint(1)   NOT NULL DEFAULT '0' COMMENT 'Download enabled from the source for this zone',
+  `is_download_enabled`                     tinyint      NOT NULL DEFAULT '0' COMMENT 'Download enabled from the source for this zone',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unicity` (`plugin_carbon_carbonintensitysources_id`, `plugin_carbon_carbonintensityzones_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
