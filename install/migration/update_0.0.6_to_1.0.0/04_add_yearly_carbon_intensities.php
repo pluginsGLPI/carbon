e6b9efa528d27aae3d5fa592a3dd4dc023713fc4
Thierry Bugier <tbugier@teclib.com>
2025-07-30 09:48:35 +0200

    feat(CarbonIntensity): add yearly intensity for most countries

--- install/migration/update_0.0.6_to_1.0.0/04_add_yearly_carbon_intensities.php
+++ install/migration/update_0.0.6_to_1.0.0/04_add_yearly_carbon_intensities.php
@@ -97,7 +97,8 @@ if ($handle = fopen($data_source, 'r')) {
 }
 
 $source_id = Install::getOrCreateSource('Hydro Quebec');
-$zone_id_quebec = Install::getOrCreateZone('Quebec', $source_id);
+$zone_id = Install::getOrCreateZone('Quebec', $source_id);
+Install::linkSourceZone($source_id, $zone_id);
 
 $quebec_carbon_intensity = include(dirname(__DIR__, 2) . '/data/carbon_intensity/quebec.php');
 foreach ($quebec_carbon_intensity as $year => $intensity) {
@@ -106,14 +107,14 @@ foreach ($quebec_carbon_intensity as $year => $intensity) {
         'FROM' => $table,
         'WHERE' => [
             'plugin_carbon_carbonintensitysources_id' => $source_id,
-            'plugin_carbon_zones_id' => $zone_id_quebec,
+            'plugin_carbon_zones_id' => $zone_id,
         ]
     ]);
     if ($result->count() === 0) {
         $success = $DB->insert($table, [
             'date' => "$year-01-01 00:00:00",
             'plugin_carbon_carbonintensitysources_id' => $source_id,
-            'plugin_carbon_zones_id' => $zone_id_quebec,
+            'plugin_carbon_zones_id' => $zone_id,
             'intensity' => $intensity,
             'data_quality' => 2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
         ]);
