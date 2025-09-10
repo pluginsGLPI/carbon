fc2fb6d518415ae253d3d36e0493be618e0a0d53
Thierry Bugier <tbugier@teclib.com>
2025-09-10 14:51:43 +0200

    bump(1.0.0) version 1.0.0

--- setup.php
+++ setup.php
@@ -41,7 +41,7 @@ use Location as GlpiLocation;
 use Profile as GlpiProfile;
 use GlpiPlugin\Carbon\Dashboard\Grid;
 
-define('PLUGIN_CARBON_VERSION', '1.0.0-beta.3');
+define('PLUGIN_CARBON_VERSION', '1.0.0');
 define('PLUGIN_CARBON_SCHEMA_VERSION', '1.0.0');
 
 // Minimal GLPI version, inclusive
