a4869bb8827fe6eee66e76763460a50c7c3fbdc1
Thierry Bugier <tbugier@teclib.com>
2025-09-10 15:21:18 +0200

    docs: fix header

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,28 +1,30 @@
 /**
  * -------------------------------------------------------------------------
- * localeoverride plugin for GLPI
+ * Carbon plugin for GLPI
+ *
+ * @copyright Copyright (C) 2024-2025 Teclib' and contributors.
+ * @license   https://www.gnu.org/licenses/gpl-3.0.txt GPLv3+
+ * @link      https://github.com/pluginsGLPI/carbon
+ *
  * -------------------------------------------------------------------------
  *
  * LICENSE
  *
- * This file is part of localeoverride.
+ * This file is part of Carbon plugin for GLPI.
  *
- * localeoverride is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * localeoverride is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with localeoverride. If not, see <http://www.gnu.org/licenses/>.
- * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2020-2022 by Teclib'.
- * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- * @link      https://services.glpi-network.com
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  * -------------------------------------------------------------------------
  */
 
