0539f4113c7c42583bb0549e7f8df0cbc6bff3d1
Thierry Bugier <tbugier@teclib.com>
2025-09-19 10:47:27 +0200

    bump(1.2.0-dev): 1.2.0-dev

---
+++ install/migration/update_1.1.0_to_1.2.0/01_rename_carbonintensitysource.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * -------------------------------------------------------------------------
+ * Carbon plugin for GLPI
+ *
+ * @copyright Copyright (C) 2024-2025 Teclib' and contributors.
+ * @license   https://www.gnu.org/licenses/gpl-3.0.txt GPLv3+
+ * @link      https://github.com/pluginsGLPI/carbon
+ *
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Carbon plugin for GLPI.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -------------------------------------------------------------------------
+ */
+
+/** @var DBmysql $DB */
+/** @var Migration $migration */
+
+// Rename CarbonIntensitySource
+// Move data to new table
+$old_table = 'glpi_plugin_carbon_carbonintensitysources';
+$new_table = 'glpi_plugin_carbon_sources';
+$migration->renameTable($old_table, $new_table);
+
+$old_itemtype = '\\GlpiPlugin\\Carbon\\CarbonIntensitySource';
+$new_itemtype = '\\GlpiPlugin\\Carbon\\Source';
+
+// Update display preferences
+$DB->update(DisplayPreference::getTable(), [
+    'itemtype' => $new_itemtype
+], [
+    'itemtype' => $old_itemtype
+]);
+
+// Rename CarbonIntensitySource_Zone
+$old_table = 'glpi_plugin_carbon_carbonintensitysources_zones';
+$new_table = 'glpi_plugin_carbon_sources_zones';
+$migration->renameTable($old_table, $new_table);
+
+$old_itemtype = '\\GlpiPlugin\\Carbon\\CarbonIntensitySource_Zone';
+$new_itemtype = '\\GlpiPlugin\\Carbon\\Source_Zone';
+
+// Update display preferences
+$DB->update(DisplayPreference::getTable(), [
+    'itemtype' => $new_itemtype
+], [
+    'itemtype' => $old_itemtype
+]);
