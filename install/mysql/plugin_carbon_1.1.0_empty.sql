e4040bb1f012bcfa3f64ebf48853e93591e3aae9
Thierry Bugier <tbugier@teclib.com>
2025-09-19 10:47:27 +0200

    bump(1.2.0-dev): 1.2.0-dev

--- install/mysql/plugin_carbon_1.1.0_empty.sql
+++ install/mysql/plugin_carbon_1.1.0_empty.sql
@@ -50,29 +50,29 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_carbonemissions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_carbonintensities` (
-  `id`                       int unsigned NOT NULL AUTO_INCREMENT,
-  `date`                     timestamp    NULL DEFAULT NULL,
-  `plugin_carbon_sources_id` int unsigned NOT NULL DEFAULT '0',
-  `plugin_carbon_zones_id`   int unsigned NOT NULL DEFAULT '0',
-  `intensity`                float        DEFAULT '0'   COMMENT 'gCO2eq/KWh',
-  `data_quality`             int unsigned NOT NULL DEFAULT '0' COMMENT 'DataTtacking\\AbstractTracked::DATA_QUALITY_* constants',
+  `id`                                      int unsigned NOT NULL AUTO_INCREMENT,
+  `date`                                    timestamp    NULL DEFAULT NULL,
+  `plugin_carbon_carbonintensitysources_id` int unsigned NOT NULL DEFAULT '0',
+  `plugin_carbon_zones_id`                  int unsigned NOT NULL DEFAULT '0',
+  `intensity`                               float        DEFAULT '0'   COMMENT 'gCO2eq/KWh',
+  `data_quality`                            int unsigned NOT NULL DEFAULT '0' COMMENT 'DataTtacking\\AbstractTracked::DATA_QUALITY_* constants',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unicity` (`date`, `plugin_carbon_sources_id`, `plugin_carbon_zones_id`),
+  UNIQUE KEY `unicity` (`date`, `plugin_carbon_carbonintensitysources_id`, `plugin_carbon_zones_id`),
   INDEX `date` (`date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_zones` (
-  `id`                                   int unsigned NOT NULL AUTO_INCREMENT,
-  `name`                                 varchar(255) DEFAULT NULL,
-  `entities_id`                          int unsigned NOT NULL DEFAULT '0',
-  `plugin_carbon_sources_id_historical`  int unsigned NOT NULL DEFAULT '0' COMMENT 'Source to be used for historical calculation',
+  `id`                                                 int unsigned NOT NULL AUTO_INCREMENT,
+  `name`                                               varchar(255) DEFAULT NULL,
+  `entities_id`                                        int unsigned NOT NULL DEFAULT '0',
+  `plugin_carbon_carbonintensitysources_id_historical` int unsigned NOT NULL DEFAULT '0' COMMENT 'Source to be used for historical calculation',
   PRIMARY KEY (`id`),
   UNIQUE KEY `unicity` (`name`),
   INDEX `entities_id` (`entities_id`),
-  INDEX `plugin_carbon_sources_id_historical` (`plugin_carbon_sources_id_historical`)
+  INDEX `plugin_carbon_carbonintensitysources_id_historical` (`plugin_carbon_carbonintensitysources_id_historical`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_sources` (
+CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_carbonintensitysources` (
   `id`               int unsigned NOT NULL AUTO_INCREMENT,
   `name`             varchar(255) DEFAULT NULL,
   `is_fallback`      tinyint      NOT NULL DEFAULT '0' COMMENT 'Fallback source for carbon intensity',
@@ -80,14 +80,14 @@ CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_sources` (
   UNIQUE KEY `unicity` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_sources_zones` (
+CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_carbonintensitysources_zones` (
   `id`               int unsigned NOT NULL AUTO_INCREMENT,
-  `plugin_carbon_sources_id` int unsigned NOT NULL DEFAULT '0',
+  `plugin_carbon_carbonintensitysources_id` int unsigned NOT NULL DEFAULT '0',
   `plugin_carbon_zones_id`   int unsigned NOT NULL DEFAULT '0',
   `code`                     varchar(255) DEFAULT NULL         COMMENT 'Zone identifier in the API of the source',
   `is_download_enabled`      tinyint      NOT NULL DEFAULT '0' COMMENT 'Download enabled from the source for this zone',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unicity` (`plugin_carbon_sources_id`, `plugin_carbon_zones_id`)
+  UNIQUE KEY `unicity` (`plugin_carbon_carbonintensitysources_id`, `plugin_carbon_zones_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `glpi_plugin_carbon_computertypes` (
