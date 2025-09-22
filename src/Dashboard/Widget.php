ec74224325bffdd8f84f39f707d0b045e85eb21c
Thierry Bugier <tbugier@teclib.com>
2025-09-22 15:40:59 +0200

    fix(Widget): better choice for energy scale min value

--- src/Dashboard/Widget.php
+++ src/Dashboard/Widget.php
@@ -574,6 +574,8 @@ class Widget extends GlpiDashboardWidget
         $apex_data['labels'] = $data['labels'];
         $apex_data['xaxis']['categories'] = $data['labels'];
 
+        $apex_data['yaxis'][1]['min'] = 0.8 * min(array_column($apex_data['series'][1]['data'], 'y'));
+
         return TemplateRenderer::getInstance()->render('@carbon/dashboard/graph-carbon-emission-per-month.html.twig', [
             'id' => $p['id'],
             'color' => $p['color'],
