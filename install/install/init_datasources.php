23404575c4818df44d82365d23b7e60c17b99440
Thierry Bugier <tbugier@teclib.com>
2025-09-22 10:31:25 +0200

    perf(Install): faster fallback data insertion
    use caching, factorize code, backport progress bar from 1.1.0

--- install/install/init_datasources.php
+++ install/install/init_datasources.php
@@ -31,6 +31,8 @@
  */
 
 use GlpiPlugin\Carbon\Install;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 /** @var DBmysql $DB */
 global $DB;
@@ -39,7 +41,6 @@ $source_id = Install::getOrCreateSource('RTE', 0);
 $zone_id = Install::getOrCreateZone('France', $source_id);
 
 $source_id = Install::getOrCreateSource('ElectricityMap', 0);
-
 
 $dbUtil = new DbUtils();
 $table = $dbUtil->getTableForItemType(GlpiPlugin\Carbon\CarbonIntensity::class);
@@ -50,57 +51,72 @@ $data_source = dirname(__DIR__) . '/data/carbon_intensity/carbon-intensity-elect
 // Create data source in DB
 $source_id = Install::getOrCreateSource('Ember - Energy Institute', 1);
 
-if ($handle = fopen($data_source, 'r')) {
-    $line_number = 0;
-    while (($line = fgetcsv($handle, 255, ',', '"', '\\')) !== false) {
-        $line_number++;
-        if ($line_number === 1 || count($line) < 4) {
-            continue; // Skip header or  lines with insufficient data
-        }
+$zone_for_ember = [];
 
-        $entity = $line[0];
-        $code = $line[1];
-        $year = (int)$line[2];
-        $intensity = (float)$line[3];
-
-        // Skip if the code is empty
-        if ($code === '') {
-            continue;
-        }
-
-        $zone_id = Install::getOrCreateZone($entity, $source_id);
-        Install::linkSourceZone($source_id, $zone_id);
-
-        // Insert into the database
-        $row_exists = (new DbUtils())->countElementsInTable($table, [
-            'date' => "$year-01-01 00:00:00",
-            'plugin_carbon_carbonintensitysources_id' => $source_id,
-            'plugin_carbon_zones_id' => $zone_id
-        ]);
-        if (!$row_exists) {
-            $success = $DB->insert($table, [
-                'date' => "$year-01-01 00:00:00",
-                'plugin_carbon_carbonintensitysources_id' => $source_id,
-                'plugin_carbon_zones_id' => $zone_id,
-                'intensity' => $intensity,
-                'data_quality' => 2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
-            ]);
-        } else {
-            $success = $DB->update($table, [
-                'intensity' => $intensity,
-                'data_quality' => 2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
-            ], [
-                'date' => "$year-01-01 00:00:00",
-                'plugin_carbon_carbonintensitysources_id' => $source_id,
-                'plugin_carbon_zones_id' => $zone_id
-            ]);
-        }
-
-        if (!$success) {
-            throw new \RuntimeException("Failed to insert data for year $year");
-        }
-    }
+try {
+    $file = new SplFileObject($data_source, 'r');
+} catch (\RuntimeException $e) {
+    throw $e;
+} catch (\LogicException $e) {
+    throw $e;
 }
+
+// Prepare the progress bar
+$file->setFlags(SplFileObject::READ_CSV);
+$progress_bar = null;
+$rows_count = 0;
+if (isCommandLine()) {
+    // Count the rows to process
+    $file->seek(PHP_INT_MAX); // Go to the end of the file
+    $rows_count = $file->key() - 1; // Get the line number ignoring headers line (aka count rows)
+    $file->rewind();
+
+    $output = new ConsoleOutput();
+    $output->writeln("Writing fallback carbon intensity data");
+    $progress_bar = new ProgressBar($output, $rows_count);
+}
+
+$line_number = 0;
+while (is_array($line = $file->fgetcsv(',', '"', '\\'))) {
+    $line_number++;
+    if ($line_number === 1 || count($line) < 4) {
+        continue; // Skip header or  lines with insufficient data
+    }
+    if ($progress_bar) {
+        $progress_bar->advance();
+    }
+
+    $entity = $line[0];
+    $code = $line[1];
+    $year = (int)$line[2];
+    $intensity = (float)$line[3];
+
+    // Skip if the code is empty
+    if ($code === '') {
+        continue;
+    }
+
+    $zone_id = $zone_for_ember[$entity] ?? null;
+    if ($zone_id === null) {
+        $zone_id = Install::getOrCreateZone($entity, $source_id);
+        $zone_for_ember[$entity] = $zone_id;
+        Install::linkSourceZone($source_id, $zone_id);
+    }
+
+    // Insert into the database
+    Install::insertOrUpdateCarbonIntensity(
+        "$year-01-01 00:00:00",
+        $source_id,
+        $zone_id,
+        $intensity,
+        2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
+    );
+}
+$file = null; // close the file
+if (isCommandLine()) {
+    $progress_bar->setProgress($rows_count);
+}
+unset($zone_for_ember);
 
 $source_id = Install::getOrCreateSource('Hydro Quebec');
 $zone_id_quebec = Install::getOrCreateZone('Quebec', $source_id);
@@ -108,27 +124,11 @@ Install::linkSourceZone($source_id, $zone_id);
 
 $quebec_carbon_intensity = include(dirname(__DIR__) . '/data/carbon_intensity/quebec.php');
 foreach ($quebec_carbon_intensity as $year => $intensity) {
-    $row_exists = (new DbUtils())->countElementsInTable($table, [
-        'date' => "$year-01-01 00:00:00",
-        'plugin_carbon_carbonintensitysources_id' => $source_id,
-        'plugin_carbon_zones_id' => $zone_id_quebec,
-    ]);
-    if (!$row_exists) {
-        $success = $DB->insert($table, [
-            'date' => "$year-01-01 00:00:00",
-            'plugin_carbon_carbonintensitysources_id' => $source_id,
-            'plugin_carbon_zones_id' => $zone_id_quebec,
-            'intensity' => $intensity,
-            'data_quality' => 2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
-        ]);
-    } else {
-        $success = $DB->update($table, [
-            'intensity' => $intensity,
-            'data_quality' => 2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
-        ], [
-            'date' => "$year-01-01 00:00:00",
-            'plugin_carbon_carbonintensitysources_id' => $source_id,
-            'plugin_carbon_zones_id' => $zone_id
-        ]);
-    }
+    Install::insertOrUpdateCarbonIntensity(
+        "$year-01-01 00:00:00",
+        $source_id,
+        $zone_id_quebec,
+        $intensity,
+        2 // constant GlpiPlugin\Carbon\DataTracking::DATA_QUALITY_ESTIMATED
+    );
 }
